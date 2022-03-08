f706cf7ba48513df2d7f51bc6dd53840942086a4
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-09 02:02:34 +0600

    add mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const port = process.env.PORT || 5555
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5555;
 
-const app = express()
+// MongoDB Connection
+connectDB();
+
+const app = express();
 
 // Middleware
 // @desc this helps to pass objects through request
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // APIs
-app.use('/api/v1/goals', require('./routes/goalRoutes'))
+app.use("/api/v1/goals", require("./routes/goalRoutes"));
 
-app.get('/', (req, res)=> {
-    res.status(200).json({
-        message : 'This is the Root'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "This is the Root",
+  });
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, ()=> console.log(`Server Started on ${port}`))
+app.listen(port, () => console.log(`Server Started on ${port}`));
