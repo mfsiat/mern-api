1d1bc3408787e4101c502ecb4ecfc29e7335ddba
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-09 20:11:54 +0600

    add model for goals

---
+++ backend/model/goalModel.js
@@ -0,0 +1,15 @@
+// @desc this is the model this defines the relationship
+const mongoose = require("mongoose");
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Goal", goalSchema);
