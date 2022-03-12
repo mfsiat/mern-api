1be2c5d30ad760e1ded6cf9ea7d3955753211373
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-13 00:30:59 +0600

    user password hashed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,18 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('User already exists')
   }
+
+  // hash password 
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // create the user 
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+  
   res.json({ message: "Register User" });
 });
 
