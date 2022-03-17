89609bb1a2c3526abab5b1ac2d046a094270a25e
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-17 17:32:57 +0600

    code refactor

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,8 +3,6 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../model/userModel");
 
-
-
 // @desc Register new user
 // @route POST /api/v1/users
 // @access Public
@@ -21,65 +19,61 @@ const registerUser = asyncHandler(async (req, res) => {
   // check if user exists
   const userExists = await User.findOne({ email });
 
-  if(userExists) {
-    res.status(400)
-    throw new Error('User already exists')
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
   }
 
-  // hash password 
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  // create the user 
+  // create the user
   const user = await User.create({
     name,
     email,
-    password: hashedPassword
-  })
+    password: hashedPassword,
+  });
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id)
-    })
+      token: generateToken(user._id),
+    });
   } else {
-    res.status(400)
-    throw new Error('Invalid user data')
+    res.status(400);
+    throw new Error("Invalid user data");
   }
 
   // res.json({ message: "Register User" });
 });
 
-
-
 // @desc Login new user
 // @route POST /api/v1/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
   // Check for user email
-  const user = await User.findOne({email})
+  const user = await User.findOne({ email });
 
-  // compare password 
-  if(user && (await bcrypt.compare(password, user.password))) {
+  // compare password
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
       _id: user.id,
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
-    })
+    });
   } else {
-    res.status(400)
-    throw new Error('Invalid credentials')
+    res.status(400);
+    throw new Error("Invalid credentials");
   }
 
   // res.json({ message: "Login User" });
 });
-
-
 
 // @desc  Get User Data
 // @route GET /api/v1/me
@@ -88,12 +82,12 @@ const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data display" });
 });
 
-// Generate JWT 
+// Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  })
-}
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
