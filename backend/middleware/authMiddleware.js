805803b6d93075dedadbe005f952cb16a03b3f63
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-18 17:37:29 +0600

    add authMiddleware, init requirements

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {});
+
+module.exports(protect);
