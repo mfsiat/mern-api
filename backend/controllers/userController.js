07ccb794ad602fa72fa57296a66475a443e22d5d
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-18 17:30:47 +0600

    changed getUserData to private from public

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc  Get User Data
 // @route GET /api/v1/me
-// @access Public
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data display" });
 });
