ca24093c5c5811971a48c515fa741a6b93abc739
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-23 03:49:28 +0600

    add auth middleware on getGoals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,9 +7,13 @@ const {
   delGoals,
 } = require("../controllers/goalController");
 
+// add auth protection 
+// for getting users individual goals 
+const { protect } = require('../middleware/authMiddleware')
+
 // Get & Post
 // debugger;
-router.route("/").get(getGoals).post(setGoals);
+router.route("/").get(protect, getGoals).post(setGoals);
 // Update & Delete
 router.route("/:id").delete(delGoals).put(updateGoals);
 
