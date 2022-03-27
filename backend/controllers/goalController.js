f1786e72a3cef7e192f5230bf0f67cd61473a1db
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-28 00:13:38 +0600

    add feature: prevent users from updating other users goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../model/goalModel");
+const User = require('../model/userModel')
 
 // @desc Get goals
 // @route GET // api/v1/goals
@@ -40,6 +41,21 @@ const updateGoals = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  // Prevent users to update other users goals
+  const user = await User.findById(req.user.id)
+
+  // Check for user 
+  if(!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  // Make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
