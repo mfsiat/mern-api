e1b95996e59c9cfaa06e06a34b7dd8bc5537ac94
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-04-22 21:21:38 +0600

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className='container'>
       <Routes>
         <Route path='/' element={Dashboard} />
+        <Route path='/login' element={Login} />
+        <Route path='/register' element={Register} />
       </Routes>
     </div>
     </Router>
