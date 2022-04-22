6b7b54cf08da26b5aa0ddc73b11f6c9b4dfbe6c9
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-04-22 21:21:05 +0600

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <Router>
+    <div className='container'>
+      <Routes>
+        <Route path='/' element={Dashboard} />
+      </Routes>
     </div>
+    </Router>
   )
 }
 
