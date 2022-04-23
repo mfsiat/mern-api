cb1d12cad1343c52a0371341655a19bc4e5c7261
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-04-24 02:39:50 +0600

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
-import Register from './pages/Register'
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
-    <div className='container'>
-      <Routes>
-        <Route path='/' element={Dashboard} />
-        <Route path='/login' element={Login} />
-        <Route path='/register' element={Register} />
-      </Routes>
-    </div>
-    </Router>
-  )
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
