ed9cdb17224e16d2df077c758ec65deea10f3988
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-04-25 01:31:53 +0600

    init state and useEffect

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,15 @@
+import { useState } from 'react'
+
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {name, email, password, password2} = formData
+  
   return (
     <div>Register</div>
   )
