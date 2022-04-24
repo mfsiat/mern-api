5ee2adc789b571e73f524bb95f4dffd8f1b8798c
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-04-25 01:36:57 +0600

    fromated code

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,31 +1,41 @@
-import { useState } from 'react'
-import { FaUser } from 'react-icons/fa'
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
 
 function Register() {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: ''
-  })
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-  const {name, email, password, password2} = formData
-  
-  const onChange = () => {}
+  const { name, email, password, password2 } = formData;
 
-  return <>
-    <section className='heading'>
-      <h1>
-        <FaUser/> Register
-      </h1>
-      <p>Please Create an account</p>
-    </section>
-    <section className="form">
-      <form>
-        <input type="text" className="form-control" id='name' name='name' value={name} placeholder='Enter Your Name' onChange={onChange} />
-      </form>
-    </section>
-  </>
+  const onChange = () => {};
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please Create an account</p>
+      </section>
+      <section className="form">
+        <form>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={name}
+            placeholder="Enter Your Name"
+            onChange={onChange}
+          />
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
