a3d0cc4d4daaf2e08223712b849d1a5223b22b67
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-05-24 19:45:00 +0600

    login function implementation on authSlice and authService

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -22,6 +22,17 @@ export const register = createAsyncThunk('auth/register', async(user, thunkAPI) 
   }
 })
 
+// Login user
+export const login = createAsyncThunk('auth/login', async(user, thunkAPI) => {
+  try {
+    return await authService.login(user)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+
 // log out 
 export const logout = createAsyncThunk('auth/logout', async () => {
   await authService.logout()
