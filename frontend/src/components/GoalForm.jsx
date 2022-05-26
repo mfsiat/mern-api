d9348cb3329725f3e270ee7243cfb04df7d380c4
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-05-27 04:11:38 +0600

    goalform add

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 function GoalForm() {
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>GoalForm from Siat</div>
-  )
+    <section className="form">
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="text">Goal</label>
+          <input
+            type="text"
+            name="text"
+            id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-block" type="submit">
+            Add Goal
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default GoalForm
+export default GoalForm;
