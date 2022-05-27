bc4d08cdb3ef7c1fb9d3b367612eac7c25dc6d3e
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-05-27 17:53:47 +0600

    working on authService createGoal done

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,2 +1,24 @@
+import axios from 'axios'
 
 const API_URL = '/api/v1/goals/'
+
+// Create new goal 
+// pass the token
+const createGoal = async (goalData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  // get the reponse
+  const response = await axios.post(API_URL, goalData, config)
+  // return the response
+  return response.data
+}
+
+const goalService = {
+  createGoal
+}
+
+export default goalService
