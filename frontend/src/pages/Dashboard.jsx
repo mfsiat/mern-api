8bafc17f3a6ffd2a440d18c3143f04355e54ffe9
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-05-27 19:59:53 +0600

    display user created goals on dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
 
@@ -42,6 +43,16 @@ function Dashboard() {
       </section>
 
       <GoalForm />
+      {/* display the goals */}
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (<h3>You have not set any goals</h3>)}
+      </section>
     </>
   )
 }
